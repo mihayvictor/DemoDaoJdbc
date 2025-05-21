fdebb58c3c7a0e4f8cc0261e523269694e086bb4
mihayvictor <m.victorcezario@gmail.com>
2025-05-21 19:58:01 -0300

    Departament  entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Departement;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Departement obj = new Departement(1, "Book");
+		System.out.println(obj);
+
+	}
+
+}
